a60c2cbfd8fa6b43e7e10e0945d63522e5a94c86
Darya Belousova <belousmind@gmail.com>
2024-02-11 00:55:55 +0100

    feat: solve convertNumberToString

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -137,28 +137,58 @@ function convertToRomanNumerals(/* num */) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
-  // const nums = {
-  //   "0": "zero",
-  //   "1": "one",
-  //   "2": "two",
-  //   "3": "three",
-  //   "4": "four",
-  //   "5": "five",
-  //   "6": "six",
-  //   "7": "seven",
-  //   "8": "eight",
-  //   "9": "nine",
-  //   ".": "point",
-  //   ",": "point",
-  //   "-": "minus",
-  // }
-  // console.log(Object.keys(nums))
-  // const newStr = [];
-  // for (let i = 0; i < numberStr.length; i += 1) {
-  //   console.log(Object.keys(nums))
-  // }
+function convertNumberToString(numberStr) {
+  let result = '';
+  for (let i = 0; i < numberStr.length; i += 1) {
+    const digit = numberStr[i];
+    switch (digit) {
+      case '0':
+        result += 'zero ';
+        break;
+      case '1':
+        result += 'one ';
+        break;
+      case '2':
+        result += 'two ';
+        break;
+      case '3':
+        result += 'three ';
+        break;
+      case '4':
+        result += 'four ';
+        break;
+      case '5':
+        result += 'five ';
+        break;
+      case '6':
+        result += 'six ';
+        break;
+      case '7':
+        result += 'seven ';
+        break;
+      case '8':
+        result += 'eight ';
+        break;
+      case '9':
+        result += 'nine ';
+        break;
+      case '.':
+        result += 'point ';
+        break;
+      case ',':
+        result += 'point ';
+        break;
+      case '-':
+        result += 'minus ';
+        break;
+      default:
+    }
+  }
+  let sentense = '';
+  for (let i = 0; i < result.length - 1; i += 1) {
+    sentense += result[i];
+  }
+  return sentense;
 }
 
 /**
